0f8377f1e5594f7036da3cf1d3a635e6df0eb939
Pontus Johansson <johanssonp998@gmail.com>
2026-05-09 17:39:34 +0200

    fyller i fetch anropet för profilen / get. skriver ut information i UI:t

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -11,8 +11,37 @@ if(!token) {
 // Hämtar profildata från skyddad route när sidan laddas
 async function loadProfile(){
     try {
+        const response = await fetch(`${API_URL}/profile`, {
+            method: "GET",
+            headers: {
+                "authorization": `Bearer ${token}`
+            }
+        });
+
+        const data = await response.json();
+
+        if(response.ok) {
+            // Fylla i profildata
+            document.getElementById("profile-username").textContent = data.user.username;
+            document.getElementById("profile-user").textContent = data.user.username;
+            document.getElementById("profile-email").textContent = data.user.email;
+
+            // Created date
+            const created = new Date(data.user.account_created);
+            document.getElementById("profile-created").textContent = created.toLocaleDateString("sv-SE");
+        }
 
     } catch (error) {
         console.error(error);
     }
 }
+
+// Eventlyssnare på logga ut knappen för att radera session token.
+// När token raderat redirect till login-sidan
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    sessionStorage.removeItem("token");
+    window.location.href = "/login";
+});
+
+// Ladda profilsidan.
+loadProfile();
